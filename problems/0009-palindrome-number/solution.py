b452ee64da973d164bd5bfb39f1fc00ece17c28e
yijiun <yijiunchin@gmail.com>
2023-09-03 13:52:18 +0000

    feat: LeetCode Sync Runtime - 50 ms (5.22%), Memory - 16.4 MB (100.00%)

--- problems/0009-palindrome-number/solution.py
+++ problems/0009-palindrome-number/solution.py
@@ -1,9 +1,7 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        x_str = str(x)
-        count = 1
-        for item in x_str[:len(x_str)//2]:
-            if not item == x_str[-count]:
-                return False
-            count += 1
-        return True
+        if x < 0:
+            return False
+
+        return str(x) == str(x)[::-1]
+
