74a39a4438d741dc95c6d49fe71db2b2a3cad97d
yijiun <yijiunchin@gmail.com>
2026-01-06 15:32:26 +0000

    feat: LeetCode Sync - Maximum Level Sum of a Binary Tree - Runtime - 12 ms (89.93%), Memory - 22.9 MB (13.07%)

---
+++ problems/1116-maximum-level-sum-of-a-binary-tree/solution.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        max_sum = float('-inf')
+        max_level = 1
+        level = 1
+        queue = deque([root])
+
+        while queue:
+            level_sum = 0
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level_sum += node.val
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            if level_sum > max_sum:
+                max_sum = level_sum
+                max_level = level
+            
+            level += 1
+            
+        return max_level
+
