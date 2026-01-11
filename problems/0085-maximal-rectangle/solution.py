9e2a351464f24ffa924a058620f4245ed0426c2a
yijiun <yijiunchin@gmail.com>
2026-01-11 18:23:54 +0000

    feat: LeetCode Sync - Maximal Rectangle - Runtime - 27 ms (96.65%), Memory - 24.6 MB (8.87%)

---
+++ problems/0085-maximal-rectangle/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maximalRectangle(self, matrix: list[list[str]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+
+        n = len(matrix[0])
+        heights = [0] * (n + 1)
+        area_max = 0
+
+        for row in matrix:
+            for i in range(n):
+                heights[i] = heights[i] + 1 if row[i] == '1' else 0
+
+            stack = [-1]
+            for i in range(n + 1):
+                while heights[i] < heights[stack[-1]]:
+                    h = heights[stack.pop()]
+                    w = i - stack[-1] - 1
+                    area_max = max(area_max, h * w)
+                stack.append(i)
+
+        return area_max
+
