f3db8778d427e4cbb677d5ec4ea589e6894ebab1
yijiun <yijiunchin@gmail.com>
2026-02-17 09:07:11 +0000

    feat: LeetCode Sync - Binary Watch - Runtime - 1 ms (41.09%), Memory - 19.4 MB (60.29%)

---
+++ problems/0401-binary-watch/solution.py
@@ -0,0 +1,3 @@
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        return [f"{h}:{m:02d}" for h in range(12) for m in range(60) if (bin(h) + bin(m)).count('1') == turnedOn]
