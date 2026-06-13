e6d9ce1c922aecb8c204f6de50d173109fc6715b
yijiun <yijiunchin@gmail.com>
2026-06-13 09:20:42 +0000

    feat: LeetCode Sync - Weighted Word Mapping - Runtime - 7 ms (82.33%), Memory - 19.3 MB (72.25%)

--- problems/4216-weighted-word-mapping/solution.py
+++ problems/4216-weighted-word-mapping/solution.py
@@ -2,6 +2,10 @@ class Solution:
     def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
         ans = ''
         for s in words:
-            ans += chr(122 - sum(weights[ord(c) - 97] for c in s) % 26)
+            n = 0
+            for c in s:
+                n += weights[ord(c) - 97]
+
+            ans += chr(122 - n % 26)
 
         return ans
