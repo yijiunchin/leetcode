19654153bd5561d453c5dd54c19ab4646ef4da84
yijiun <yijiunchin@gmail.com>
2026-06-14 17:35:03 +0000

    feat: LeetCode Sync - Maximum Twin Sum of a Linked List - Runtime - 53 ms (82.96%), Memory - 63.3 MB (6.58%)

---
+++ problems/2236-maximum-twin-sum-of-a-linked-list/solution.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        vals = []
+        while head:
+            vals.append(head.val)
+            head = head.next
+        return max(vals[i] + vals[~i] for i in range(len(vals) // 2))
