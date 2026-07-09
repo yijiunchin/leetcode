1b101d054f617926f8c0a83deba376139f984414
yijiun <yijiunchin@gmail.com>
2026-07-09 06:58:57 +0000

    feat: LeetCode Sync - Path Existence Queries in a Graph I - Runtime - 98 ms (59.52%), Memory - 50.4 MB (39.68%)

---
+++ problems/3838-path-existence-queries-in-a-graph-i/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
+        comp = [0] * n
+        for i in range(1, n):
+            comp[i] = comp[i - 1] + (nums[i] - nums[i - 1] > maxDiff)
+        return [comp[u] == comp[v] for u, v in queries]
