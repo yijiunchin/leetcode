d588ea44a3d1e6139803e6c554cbe13faab8675a
yijiun <yijiunchin@gmail.com>
2026-08-01 00:25:34 +0000

    feat: LeetCode Sync - Predict the Winner - Runtime - 4 ms (39.38%), Memory - 20.1 MB (6.68%)

---
+++ problems/0486-predict-the-winner/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        @cache
+        def dp(i, j):
+            if i == j:
+                return nums[i]
+            return max(nums[i] - dp(i + 1, j), nums[j] - dp(i, j - 1))
+            
+        return dp(0, len(nums) - 1) >= 0
