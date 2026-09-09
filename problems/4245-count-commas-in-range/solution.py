031aa24ac847dfe97c83815ce6834a3121f5f4da
yijiun <yijiunchin@gmail.com>
2026-09-09 15:10:04 +0000

    feat: LeetCode Sync - Count Commas in Range - Runtime - 0 ms (100.00%), Memory - 19.2 MB (46.52%)

---
+++ problems/4245-count-commas-in-range/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def countCommas(self, n: int) -> int:
+        return sum(
+            max(0, n - 10 ** (3 * i) + 1)
+            for i in range(1, len(str(n)) // 3 + 1)
+        )
